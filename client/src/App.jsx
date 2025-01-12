85ba027bcbe15f2c18f661be7c696ff5f38a3dec
Veikka <veikka.nevala@outlook.com>
2025-01-12 15:12:02 +0200

    buttons on map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import dust2 from "../../maps/dust2.jpg"
 import { TransformWrapper, 
-  TransformComponent } from "react-zoom-pan-pinch"
+  TransformComponent, } from "react-zoom-pan-pinch"
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = (event) => {
+    console.log("TODO")
+  }
+
   return (
     <div>
       <h1>CSutil</h1>
-      <TransformWrapper>
+      <TransformWrapper doubleClick={{disabled: true}}>
         <TransformComponent>
-          <img src={dust2}></img>
+          <img src={dust2} onClick={handleClick}></img>
+          <button style={{left: "245px", top: "225px"}} 
+          onClick={handleClick}>test</button>
         </TransformComponent>
       </TransformWrapper>
     </div>
