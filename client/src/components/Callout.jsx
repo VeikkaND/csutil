c5fafb367d8a1079d7e9155401fc14f3e8867050
Veikka <veikka.nevala@outlook.com>
2025-01-19 17:22:14 +0200

    callout size parameter

--- client/src/components/Callout.jsx
+++ client/src/components/Callout.jsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux"
 
-function Callout({left, top, name}) {
+function Callout({left, top, name, size}) {
     const calloutsEnabled = useSelector(
         (state) => state.callouts.value)
 
     return(
         <label style={{left: `${left}px`, top: `${top}px`, 
-            display: calloutsEnabled ? "block" : "none"}} 
+            display: calloutsEnabled ? "block" : "none", 
+            fontSize: size}} 
             id='callout'>{name}</label>
     )
 }
--- client/src/routes/Dust2.jsx
+++ client/src/routes/Dust2.jsx
@@ -37,7 +37,7 @@ function Dust2() {
                 <Callout left={365} top={300} name={"MID DOORS"}/>
                 <Callout left={510} top={170} name={"CT SPAWN"}/>
                 <Callout left={410} top={370} name={"XBOX"}/>
-                <Callout left={500} top={350} name={"SHORT"}/>
+                <Callout left={480} top={340} name={"SHORT"} size={30}/>
                 <Callout left={260} top={355} name={"LOWER TUNNELS"}/>
                 <Callout left={545} top={300} name={"STAIRS"}/>
 
@@ -47,7 +47,7 @@ function Dust2() {
                 <Callout left={730} top={58} name={"BARRELS"}/>
                 <Callout left={760} top={140} name={"RAMP"}/>
                 <Callout left={835} top={210} name={"CAR"}/>
-                <Callout left={755} top={330} name={"LONG"}/>
+                <Callout left={740} top={330} name={"LONG"} size={30}/>
                 <Callout left={710} top={210} name={"CROSS"}/>
                 <Callout left={610} top={170} name={"BOOST"}/>
                 <Callout left={765} top={550} name={"PIT"}/>
@@ -56,7 +56,7 @@ function Dust2() {
                 <Callout left={630} top={410} name={"BLUE"}/>
                 <Callout left={575} top={535} name={"LONG DOORS"}/>
 
-                <Callout left={100} top={410} name={"UPPER TUNNELS"}/>
+                <Callout left={75} top={405} name={"UPPER TUNNELS"} size={23}/>
                 <Callout left={90} top={600} name={"OUTSIDE TUNNELS"}/>
                 <Callout left={160} top={700} name={"T PLAT"}/>
 
@@ -64,9 +64,9 @@ function Dust2() {
                 <Callout left={440} top={560} name={"TOP MID"}/>
                 <Callout left={350} top={560} name={"GREEN"}/>
                 <Callout left={450} top={450} name={"CAT"}/>
-                <Callout left={400} top={470} name={"MID"}/>
+                <Callout left={385} top={430} name={"MID"} size={30}/>
                 <Callout left={375} top={700} name={"SUICIDE"}/>
-                <Callout left={315} top={800} name={"T SPAWN"}/>
+                <Callout left={290} top={795} name={"T SPAWN"} size={30}/>
             </TransformComponent>
         </TransformWrapper>
     </div>
