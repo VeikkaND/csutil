22941b7fac266f0526f49452f7f80db06c86fc13
Veikka <veikka.nevala@outlook.com>
2025-02-19 14:44:22 +0200

    ancient, anubis callouts

--- client/src/routes/Ancient.jsx
+++ client/src/routes/Ancient.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import ToolBar from "../components/Toolbar";
 import Tutorial from "../components/Tutorial";
 import ancient from "../../../maps/ancient.png"
+import Callout from "../components/Callout";
 
 function Ancient() {
 
@@ -14,6 +15,42 @@ function Ancient() {
             <TransformWrapper doubleClick={{disabled: true}}>
                 <TransformComponent wrapperStyle={{margin: "auto"}}>
                     <img src={ancient}></img>
+
+                    <Callout left={70} top={135} name={"PLAT"}/>
+                    <Callout left={90} top={160} name={"SINGLE"} size={13}/>
+                    <Callout left={130} top={195} name={"BIG BOX"} size={13}/>
+                    <Callout left={250} top={94} name={"TEMPLE"} size={25}/>
+                    <Callout left={70} top={340} name={"A MAIN"} size={25}/>
+                    <Callout left={130} top={280} name={"BOOST"}/>
+                    <Callout left={210} top={365} name={"DONUT"} size={25}/>
+                    <Callout left={330} top={155} name={"CT"} size={20}/>
+                    <Callout left={402} top={60} name={"CT SPAWN"} size={30}/>
+
+                    <Callout left={390} top={810} name={"T SPAWN"} size={30}/>
+                    <Callout left={125} top={495} name={"STAIRS"}/>
+                    <Callout left={230} top={650} name={"SPLIT"} size={20}/>
+                    <Callout left={400} top={350} name={"TOP MID"} size={20}/>
+                    <Callout left={425} top={450} name={"MID"} size={25}/>
+                    <Callout left={500} top={410} name={"PIT"}/>
+                    <Callout left={300} top={520} name={"ELBOW"} size={20}/>
+                    <Callout left={424} top={290} name={"RED"}/>
+                    <Callout left={570} top={200} name={"ALLEY"} size={20}/>
+
+                    <Callout left={680} top={250} name={"SHORT"} size={20}/>
+                    <Callout left={820} top={250} name={"LONG"} size={20}/>
+                    <Callout left={710} top={355} name={"PILLAR"}/>
+                    <Callout left={680} top={435} name={"WOOD"}/>
+                    <Callout left={623} top={355} name={"CAVE"} size={25}/>
+                    <Callout left={810} top={460} name={"RAMP"} size={25}/>
+                    <Callout left={770} top={560} name={"DOORS"}/>
+                    <Callout left={570} top={470} name={"CHEETAH"}/>
+                    <Callout left={500} top={520} name={"HEAVEN"}/>
+                    <Callout left={770} top={620} name={"RUINS"} rotate={-30}/>
+                    <Callout left={550} top={730} name={"TUNNEL"}/>
+                    <Callout left={665} top={700} name={"WATER"}/>
+
+                    <Callout left={230} top={200} name={"A"}/>
+                    <Callout left={770} top={350} name={"B"}/>
                 </TransformComponent>
             </TransformWrapper>
         </div>
--- client/src/routes/Anubis.jsx
+++ client/src/routes/Anubis.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import ToolBar from "../components/Toolbar";
 import Tutorial from "../components/Tutorial";
 import anubis from "../../../maps/anubis.png"
+import Callout from "../components/Callout";
 
 function Anubis() {
 
@@ -14,6 +15,48 @@ function Anubis() {
             <TransformWrapper doubleClick={{disabled: true}}>
                 <TransformComponent wrapperStyle={{margin: "auto"}}>
                     <img src={anubis}></img>
+
+                    <Callout left={310} top={180} name={"CT SPAWN"} size={30}/>
+                    <Callout left={400} top={80} name={"BEACH"} size={25}/>
+                    <Callout left={510} top={210} name={"TUNNEL"} size={20} rotate={30}/>
+                    <Callout left={585} top={200} name={"HEAVEN"} rotate={-50}/>
+                    <Callout left={510} top={335} name={"A CONNECTOR"}/>
+                    <Callout left={485} top={360} name={"MID"} size={25}/>
+                    <Callout left={600} top={300} name={"PLAT"} size={20}/>
+                    <Callout left={750} top={220} name={"FOUNTAIN"}/>
+                    <Callout left={690} top={175} name={"BACKSITE"}/>
+                    <Callout left={730} top={370} name={"MAIN"} size={25}/>
+
+                    <Callout left={610} top={450} name={"BOAT"} size={25}/>
+                    <Callout left={480} top={525} name={"WATER"} size={20}/>
+                    <Callout left={420} top={520} name={"BRIDGE"} rotate={-90}/>
+                    <Callout left={420} top={415} name={"WINDOW"}/>
+                    <Callout left={485} top={460} name={"DOORS"}/>
+                    <Callout left={690} top={530} name={"UPPER"} size={20}/>
+                    <Callout left={580} top={580} name={"STAIRS"}/>
+                    <Callout left={655} top={525} name={"WOOD"} size={13} rotate={-50}/>
+                    <Callout left={670} top={470} name={"DROP"} size={13}/>
+                    <Callout left={300} top={520} name={"CONNECTOR"} rotate={-30}/>
+
+                    <Callout left={245} top={240} name={"SNIPER"} rotate={-20}/>
+                    <Callout left={300} top={290} name={"CAVE"}/>
+                    <Callout left={340} top={360} name={"PALACE"} size={25}/>
+                    <Callout left={213} top={330} name={"STREET"} size={25} rotate={-90}/>
+                    <Callout left={250} top={510} name={"PILLAR"}/>
+                    <Callout left={310} top={405} name={"BACKSITE"}/>
+                    <Callout left={335} top={470} name={"NINJA"} size={13}/>
+                    <Callout left={170} top={430} name={"CORNER"}/>
+
+                    <Callout left={380} top={820} name={"T SPAWN"} size={30}/>
+                    <Callout left={200} top={560} name={"LONG"} size={25}/>
+                    <Callout left={380} top={600} name={"TOP MID"} size={25}/>
+                    <Callout left={175} top={495} name={"GATE"}/>
+                    <Callout left={135} top={490} name={"IVY"}/>
+                    <Callout left={530} top={665} name={"ALLEY"} size={25}/>
+                    <Callout left={290} top={680} name={"RUINS"} size={20}/>
+
+                    <Callout left={667} top={220} name={"A"}/>
+                    <Callout left={275} top={431} name={"B"}/>
                 </TransformComponent>
             </TransformWrapper>
         </div>
