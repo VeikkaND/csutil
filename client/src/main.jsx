164477d9c6e38571bc61a6a9a3d281e2d08e3f7d
Veikka <veikka.nevala@outlook.com>
2026-03-29 19:19:47 +0300

    changed main.jsx routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../src/index.css'
+import './index.css'
 import App from './App.jsx'
 import store from './store.js'
 import { Provider } from "react-redux"
